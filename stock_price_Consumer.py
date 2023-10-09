f67485126f9b627c0c71bc35224683e70106a8eb
tony <tonyyoung3@gmail.com>
2023-10-08 23:21:45 -0700

    save consumer data into csv

--- stock_price_Consumer.py
+++ stock_price_Consumer.py
@@ -1,4 +1,13 @@
 from confluent_kafka import Consumer, KafkaError
+import csv
+import os
+from datetime import datetime
+
+
+#tettt
+# Define the CSV file name
+csv_file = "data.csv"
+
 
 # Kafka consumer configuration
 conf = {
@@ -17,7 +26,7 @@ consumer.subscribe([topic])
 # Consume messages
 while True:
     msg = consumer.poll(1.0)
-
+    
     if msg is None:
         continue
     if msg.error():
@@ -26,4 +35,17 @@ while True:
         else:
             print(f'Error: {msg.error()}')
     else:
-        print(f'Received message: {msg.value().decode("utf-8")}')
+        print(f'received {msg.value().decode("utf-8")}')
+        data_price = [(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), msg.value().decode("utf-8"))]
+        if not os.path.exists(csv_file):
+    # If it doesn't exist, create a new file and write header and data
+            with open(csv_file, mode="w", newline="") as file:
+                writer = csv.writer(file)
+                writer.writerow(["time", "price"])  # Header
+                writer.writerows(data_price)
+        else:
+            # If it exists, open it in append mode and add data
+            with open(csv_file, mode="a", newline="") as file:
+                writer = csv.writer(file)
+                writer.writerows(data_price)
+
